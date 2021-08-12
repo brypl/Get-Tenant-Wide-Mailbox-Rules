042d4fd8842f64d82b0d96cd7607d8f4f319987e
Bryan Lam <61400370+brypl@users.noreply.github.com>
2021-08-12 11:53:18 +1000

    Create TenantWideRules-M365.ps1

---
+++ TenantWideRules-M365.ps1
@@ -0,0 +1,16 @@
+﻿Function MailboxRules-TenantWide
+{
+    $URL = "https://ps.outlook.com/powershell"
+    
+    $Credentials = Get-Credential -Message "Enter your Office 365 admin credentials"
+
+    $EXOSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $URL -Credential $Credentials -Authentication Basic -AllowRedirection -Name "Exchange Online"
+
+    Import-PSSession $EXOSession
+
+    $users = (get-mailbox -resultsize unlimited).UserPrincipalName
+    foreach ($user in $users)
+    {
+    Get-InboxRule -Mailbox $user | Select-Object MailboxOwnerID,Name,Description,Enabled,RedirectTo, MoveToFolder,ForwardTo | Export-CSV C:\Temp\365-TenancyMailboxRule.csv -NoTypeInformation -Append
+    }
+}
